ee23e7649d070dc7318e7994dd6d033e4ebfa7ac
mangelia <mangelia@cisco.com>
2022-08-02 13:04:05 -0500

    added requested features

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from urllib import request
 import requests
+import json
 
-r = requests.get("https://www.google.com")
+r = requests.get("https://dog.ceo/api/breeds/image/random")
+print(type(r))
+otra_variable = json.loads(r.text)
+print()
+print(otra_variable, type(otra_variable))
+# print(type(otra_variable))
+# print(otra_variable["message"])
+print()
+print(r.text, type(r.text))
 
-print(r.text)
+print(otra_variable["message"])
